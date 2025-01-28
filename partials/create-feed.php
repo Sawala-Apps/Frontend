353c6907f30a4ce5c4de606e8bdb36ad73555fb4
“gerry” <gerrydesrian488@gmail.com>
2025-01-28 10:28:12 +0700

    fix/textarea bg

--- partials/create-feed.php
+++ partials/create-feed.php
@@ -2,11 +2,11 @@
     <form id="postForm">
         <div class="mb-4">
             <label for="caption" class="block text-gray-800 font-bold mb-2">Caption:</label>
-            <textarea id="caption" name="caption" class="w-full p-3 border-brutal rounded-lg shadow-brutal focus:ring-2 focus:ring-blue-400 focus:outline-none resize-none" placeholder="What's on your mind?" rows="4" required></textarea>
+            <textarea id="caption" name="caption" class="w-full p-3 border-brutal rounded-lg shadow-brutal focus:ring-2 focus:ring-blue-400 focus:outline-none resize-none bg-white" placeholder="What's on your mind?" rows="4" required></textarea>
         </div>
         <div class="mb-6">
             <label for="image" class="block text-gray-800 font-bold mb-2">Upload Image:</label>
-            <div class="flex flex-row justify-center items-center max-lg:flex-col ">
+            <div class="flex flex-row justify-center items-center max-lg:flex-col mb-[-7px]">
                 <input type="file" id="image" name="image" class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-brutal file:shadow-brutal file:bg-blue-100 file:text-blue-700 hover:file:bg-blue-200" accept="image/*" required>
                 <button type="submit" class="px-6 py-2 bg-[#88AAEE] text-black font-bold rounded-lg shadow-brutal hover:bg-blue-600 border-brutal btn-brutal max-lg:mt-4 max-lg:w-[100%]">Post</button>
             </div>
