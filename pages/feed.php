0710bf5e88c996f6a296d7b20e6b6752c99d04d8
Gerry <150305559+iss4gerry@users.noreply.github.com>
2025-02-02 18:08:10 +0700

    Update feed.php

--- pages/feed.php
+++ pages/feed.php
@@ -2,28 +2,43 @@
 // static dulss
 $posts = [
     [
-        'id' => 1,
-        'content' => 'Jiji bos',
-        'author' => 'Author 1',
-        'date' => '2025-01-01 12:00:00',
-        'image_url' => 'https://i.pinimg.com/236x/70/51/16/70511629af7f37b7637ac0390dbb3207.jpg',
-        'pfp_url' => 'https://tr.rbxcdn.com/180DAY-a33f65cf6a7233cad8f393ec0350e538/420/420/Hat/Webp/noFilter'
+        'postId' => 'post1',
+        'uid' => 'user1',
+        'content_text' => 'Hello, this is my first post!',
+        'content_image' => 'https://i.ytimg.com/vi/rvX8cS-v2XM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCF0zNNCUpNmTYhJxWG7VWjmvmybA',
+        'created_at' => '2025-02-02 10:30:00',
+        'fullname' => 'tes',
+        'profile_picture' => 'https://i.redd.it/i-keep-seeing-this-angry-cat-meme-does-anyone-know-what-v0-0o96ygkg9jw91.jpg?width=1080&format=pjpg&auto=webp&s=8a7220f58ae5e5ecbef518367d48b96f7327e06d',
+        'like_count' => 12,
+        'view_count' => 250,
+        'comment_count' => 5,
+        'is_liked' => 1
     ],
     [
-        'id' => 2,
-        'content' => 'Dua bos',
-        'author' => 'Author 2',
-        'date' => '2025-01-02 13:00:00',
-        'image_url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFnczTa-dENlJBTIziOKgOPG3BEB69cyVxFg&s',
-        'pfp_url' => 'https://cdn.pfps.gg/pfps/2763-cat-face-meme.png'
+        'postId' => 'post2',
+        'uid' => 'user2',
+        'content_text' => 'tes',
+        'content_image' => 'tes',
+        'created_at' => '2025-02-01 15:45:00',
+        'fullname' => 'tes',
+        'profile_picture' => 'https://cdn.pfps.gg/pfps/2763-cat-face-meme.png',
+        'like_count' => 30,
+        'view_count' => 400,
+        'comment_count' => 10,
+        'is_liked' => 0
     ],
     [
-        'id' => 3,
-        'content' => 'Tiga bos',
-        'author' => 'Author 3',
-        'date' => '2025-01-03 14:00:00',
-        'image_url' => 'https://www.catster.com/wp-content/uploads/2023/11/Beluga-Cat-e1714190563227.webp',
-        'pfp_url' => 'https://i.pinimg.com/736x/de/a1/b2/dea1b26ed44c0a717205aa117d0bf44f.jpg'
+        'postId' => 'post3',
+        'uid' => 'user3',
+        'content_text' => 'tes',
+        'content_image' => 'https://via.placeholder.com/300',
+        'created_at' => '2025-01-30 08:15:00',
+        'fullname' => 'testes',
+        'profile_picture' => 'https://i.pinimg.com/736x/de/a1/b2/dea1b26ed44c0a717205aa117d0bf44f.jpg',
+        'like_count' => 8,
+        'view_count' => 150,
+        'comment_count' => 2,
+        'is_liked' => 1
     ]
 ];
 ?>
@@ -38,7 +53,6 @@ $posts = [
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Mega:wght@100..900&display=swap" rel="stylesheet">
 </head>
 <body class="bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px] font-sans items-center flex flex-col">
