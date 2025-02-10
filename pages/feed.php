ea3387aadd33135a6da55af1b9a278b686105036
“gerry” <gerrydesrian488@gmail.com>
2025-02-10 12:28:20 +0700

    share function

--- pages/feed.php
+++ pages/feed.php
@@ -106,7 +106,11 @@
                         <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2"/>
                     </svg>
                 </button>
-               
+               <button id="shareButton" class="btn-brutal basis-1/3 btn flex items-center justify-center space-x-2 px-4 py-2 bg-[#88AAEE] text-black font-bold rounded-md shadow-join hover:bg-yellow-600 border-brutal">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+                        <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3"/>
+                    </svg>
+                </button>
             </div>
     </div>
 
@@ -150,6 +154,29 @@
         });
     });
 
+    const shareBtn = postContainer.querySelector("#shareButton");
+    shareBtn.addEventListener('click', function (event) {
+        event.preventDefault(); 
+        event.stopPropagation();
+        const postUrl = `https://nova-agustina.my.id/22cns/John/Sawala/Frontend/pages/detail.php?postid=${post.postid}`
+        navigator.clipboard.writeText(postUrl)
+        .then(() => {
+            toastr.success("Link berhasil disalin ke clipboard!", "Sukses", {
+                    closeButton: true,
+                    progressBar: true,
+                    positionClass: "toast-top-right",
+                    timeOut: 3000
+            });
+        })
+        .catch(err => {
+            toastr.error("Gagal menyalin ke clipboard", "Error", {
+                    closeButton: true,
+                    progressBar: true,
+                    positionClass: "toast-top-right",
+                    timeOut: 3000
+                });
+        });
+    });
 
     const imagePost = postContainer.querySelector("#imagePost");
     imagePost.addEventListener('click', function () {
